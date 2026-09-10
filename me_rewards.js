a70e271922cb3d2bbba6a88764529933a2367bc4
joos7158 <dunggrun@gmail.com>
2026-09-11 00:39:31 +0900

    🎁 도장판 갱신

--- me_rewards.js
+++ me_rewards.js
@@ -7,7 +7,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "a2oUfrMpGPlQW0veGZoM5Q": {
   "stamps": 0,
@@ -16,7 +16,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "ULMoQiY8ATCj02ubEQ9Wjg": {
   "stamps": 0,
@@ -25,7 +25,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "8dW63wQ5S0IMxsxKPoFVxw": {
   "stamps": 0,
@@ -34,7 +34,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "_YkG-WNQNVqGNwdtybRA0Q": {
   "stamps": 0,
@@ -43,7 +43,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "IywvFfBf-6KH3O4MS7PqqQ": {
   "stamps": 2,
@@ -56,7 +56,7 @@ window.ME_REWARDS = {
   "gold": [
    "두 점을 지나는 직선의 방정식"
   ],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "mzsdyDG658RvaW4ZeG2qsA": {
   "stamps": 0,
@@ -65,10 +65,10 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "Z83nVYQ26xYl7HEmvvEYaQ": {
-  "stamps": 5,
+  "stamps": 6,
   "redeemed": 0,
   "per": 10,
   "gift": "편의점 5천원 기프티콘",
@@ -76,12 +76,13 @@ window.ME_REWARDS = {
    "2026-08-29",
    "2026-09-07",
    "2026-09-08",
-   "2026-09-09"
+   "2026-09-09",
+   "2026-09-11"
   ],
   "gold": [
    "두 접점을 지나는 직선 (극선)"
   ],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "PbcD92Dv87ukiUoSIhypUw": {
   "stamps": 0,
@@ -90,7 +91,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "FiX91SBiihqTfUL-dwMElg": {
   "stamps": 1,
@@ -101,7 +102,7 @@ window.ME_REWARDS = {
    "2026-08-28"
   ],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "haOPA17TodCIFf0HoU8z3A": {
   "stamps": 5,
@@ -117,7 +118,7 @@ window.ME_REWARDS = {
    "최대·최소의 활용(도형·실생활)",
    "평균값 정리를 만족하는 상수 c 구하기"
   ],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "BrCadZk4dgNpTxQg8u8qcQ": {
   "stamps": 0,
@@ -126,7 +127,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "98XssE27H04rd7StETOaYQ": {
   "stamps": 3,
@@ -140,7 +141,7 @@ window.ME_REWARDS = {
   "gold": [
    "다항함수의 도함수 구하기"
   ],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "Dq57-e2nPl01TtHsM-Xtuw": {
   "stamps": 3,
@@ -154,7 +155,7 @@ window.ME_REWARDS = {
   "gold": [
    "곡선 위의 한 점에서의 접선 방정식"
   ],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "oDUG17HlPyQHIEK4B065dw": {
   "stamps": 3,
@@ -168,7 +169,7 @@ window.ME_REWARDS = {
    "평균값 정리를 만족하는 상수 c 구하기",
    "함수의 증가와 감소"
   ],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "qyW_ZLn0AEWZmFat76kQ4w": {
   "stamps": 4,
@@ -183,7 +184,7 @@ window.ME_REWARDS = {
    "이항정리의 활용 (나머지·근삿값)",
    "부등식의 해의 개수 (여유변수)"
   ],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "L-0jFQ2UTmnO4308juedZw": {
   "stamps": 0,
@@ -192,7 +193,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "NMo53UgyQxZ-PlLwoWaQiQ": {
   "stamps": 0,
@@ -201,7 +202,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "SY8OnpfaRwmJb7pmenzZvA": {
   "stamps": 0,
@@ -210,7 +211,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "HvtR5tsgR_p5hq3lWwUEwA": {
   "stamps": 0,
@@ -219,7 +220,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "Bt0Gethtb19ScZineAI9AQ": {
   "stamps": 0,
@@ -228,7 +229,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "jBg85unLXRkRSPBO_KSSSQ": {
   "stamps": 1,
@@ -239,7 +240,7 @@ window.ME_REWARDS = {
   "gold": [
    "유리함수와 무리함수의 그래프 (교점 존재 조건)"
   ],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "Mvyuvyc4aXfF7qK_a79DZw": {
   "stamps": 0,
@@ -248,7 +249,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "FcrEQVl16vghX2b7HzpjQw": {
   "stamps": 0,
@@ -257,7 +258,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "4fWLMN7ybNWqk_IWZYr5zQ": {
   "stamps": 2,
@@ -269,7 +270,7 @@ window.ME_REWARDS = {
    "곡선 위의 한 점에서의 접선 방정식",
    "평균값 정리를 만족하는 상수 c 구하기"
   ],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "QymwTX87H7xSJtAsB2q4jg": {
   "stamps": 1,
@@ -280,7 +281,7 @@ window.ME_REWARDS = {
    "2026-09-04"
   ],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "l9KiHOI2ZJ1aRbdFn8vIgQ": {
   "stamps": 0,
@@ -289,7 +290,7 @@ window.ME_REWARDS = {
   "gift": "편의점 5천원 기프티콘",
   "days": [],
   "gold": [],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  },
  "demo": {
   "stamps": 6,
@@ -300,6 +301,6 @@ window.ME_REWARDS = {
   "gold": [
    "(예시) 두 점 사이의 거리"
   ],
-  "today": "2026-09-10"
+  "today": "2026-09-11"
  }
 };
